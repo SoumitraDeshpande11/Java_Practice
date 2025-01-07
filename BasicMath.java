425efb4c16af9d95f0f6ef82b1ffd1f0552cb4cc
Soumitra <soumitradesh11@gmail.com>
2025-01-07 12:19:41 +0530

    Add files via upload

---
+++ BasicMath.java
@@ -0,0 +1,24 @@
+class BasicMath {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+    System.out.println("Sum: " + sum);
+    System.out.println("Sub: " + sub);
+    System.out.println("Mul: " + mul);
+    System.out.println("Div: " + div);
+    System.out.println("Mod: " + mod);
+
+    double da = 1;
+    System.out.println("da: " + da);
+
+
+
+
+    }
+    
+}

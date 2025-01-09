341e822e0c79d41355dae37c80ce32fd61b85465
Soumitra <soumitradesh11@gmail.com>
2025-01-09 11:06:13 +0530

    Add files via upload

---
+++ ForEx.java
@@ -0,0 +1,13 @@
+public class ForEx {
+    public static void main(String[] args) {
+        int b;
+        int a;
+        for(a = 1,b = 6;a<b;a++,b--){
+            System.out.println(a+" "+b);
+
+        }
+
+        
+    }
+    
+}

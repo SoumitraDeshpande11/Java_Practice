a74de9c5c6fda22d2ab85b276906ca012b9fec18
Soumitra <soumitradesh11@gmail.com>
2025-01-09 09:55:08 +0530

    Add files via upload

---
+++ Q9.java
@@ -0,0 +1,8 @@
+public class Q9 {
+    public static void main(String[] args) {
+        double doubleValue = 9.78;
+        int intValue = (int) doubleValue; 
+        System.out.println("Double value: " + doubleValue);
+        System.out.println("Converted to int: " + intValue);
+    }
+}

02346fb9158c70c39becf8f1714b46ba57d0caa8
Soumitra <soumitradesh11@gmail.com>
2025-01-09 10:33:37 +0530

    Add files via upload

---
+++ DoWhileEx.java
@@ -0,0 +1,11 @@
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int i = 1;
+        do { 
+            System.err.println(i);
+            i++;
+            
+        } while (i<=10);
+    }
+    
+}

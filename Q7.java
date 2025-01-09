f9d20f7e6026a0f3ffed1be4ddb176293bbc69f4
Soumitra <soumitradesh11@gmail.com>
2025-01-09 09:54:46 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,12 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int age = 25;
+        float height = 5.8f;
+        char grade = 'A';
+        boolean isPass = true;
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPass);
+    }
+}

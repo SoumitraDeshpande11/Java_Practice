2f9e8a9e55087091f62d2dcb66a71f34d27af035
Soumitra <soumitradesh11@gmail.com>
2025-01-09 10:30:58 +0530

    Add files via upload

---
+++ WhileEx.java
@@ -0,0 +1,9 @@
+public class WhileEx {
+    public static void main(String args[]){
+        int i = 0;
+        while(i<10){
+            System.out.println("yayyyieee");
+            i++;
+        }
+    }
+}
